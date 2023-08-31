0a2401a1a61c7710fe10192fe6f58317ef8425d3
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-31 22:31:49 +0200

    finito componente TheConsultants

--- src/components/TheConsultants.vue
+++ src/components/TheConsultants.vue
@@ -49,17 +49,23 @@ export default{
 
 <!-- tempalte section -->
 <template>
-    <section class="bg-white">
-        <h2 class="pseudo mb-5">OUR CONSULTANTS CAN HELP YOU</h2>
-        <div class="container">
-            <div class="row row-cols-2">
-                <div class="col d-flex gap-3" v-for="(item, index) in consultansLinks" :key="index">
-                    <div class="icon">
-                        <a href="#"><i :class="item.icon"></i></a>
-                    </div>
-                    <div class="description">
-                        <h3>{{ item.title }}</h3>
-                        <p>{{ item.subtitle }}</p>
+    <section class="bg-white ms-5">
+        <div class="container-consultants">
+            <div class="container-title d-flex align-items-center gap-5 mb-2">
+                <h2 class="pseudo">OUR CONSULTANTS CAN HELP YOU</h2>
+                <div class="line-divider"></div>
+            </div>
+            
+            <div class="container">
+                <div class="row row-cols-2 ">
+                    <div class="col-5 d-flex gap-5 py-5" v-for="(item, index) in consultansLinks" :key="index">
+                        <div class="icon me-2 mt-3">
+                            <a class="rounded-circle p-3" href="#"><i :class="item.icon"></i></a>
+                        </div>
+                        <div class="description ms-2">
+                            <h3 class="mb-3">{{ item.title }}</h3>
+                            <p>{{ item.subtitle }}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -74,11 +80,21 @@ export default{
 <!-- style section -->
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
-.i {
-    font-size: 3rem;
+
+.line-divider{
+    height: 3px;
+    width: 798px;
+    background-color: #39ffbf;
+}
+.container-consultants{
+    padding-left: 100px;
+    padding-top: 100px;
+}
+
+a{
     color: white;
     background-color: $icon-consultants-color;
-    border-radius: 0;
+    font-size: 1.5rem;
 }
 
 </style>
