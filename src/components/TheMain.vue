ecd988547320d3110a2c6c95464697855f17d91f
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-31 18:31:57 +0200

    creato componente TheMain

---
+++ src/components/TheMain.vue
@@ -0,0 +1,37 @@
+<!-- script section -->
+<script>
+
+// import vue components codes
+
+export default{
+    // name
+    name: "TheMain",
+
+    components:{
+        
+        
+    }
+}
+</script>
+<!-- /script section -->
+
+
+
+<!-- tempalte section -->
+<template>
+    
+    <!-- main -->
+    <main>
+    
+    </main>
+    <!-- /main section -->
+
+</template>
+<!-- /template section -->
+
+
+
+<!-- style section -->
+<style lang="scss" scoped>
+</style>
+<!-- /style section -->
