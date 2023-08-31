2be5a51cc53d9290f8d13c803ae3d475b293e447
DarioEsposito89 <darioesposito.work@gmail.com>
2023-08-31 20:46:20 +0200

    svuotato sore

--- store.js
+++ store.js
@@ -5,31 +5,5 @@ import { reactive } from "vue";
 export const store = reactive({
 
     // consultans links section store data
-    consultansLinks:[
-
-        {
-            title: "STATISTICAL CONSULTING",
-            subtitle:"When while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed by when i hear the buzz of the title.",
-            icon: "fa-solid fa-chart-line"
-        },
-        
-        {
-            title: "DIGITAL CONSULTING",
-            subtitle: "When while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed by when i hear the buzz of the title.",
-            icon: "fa-solid fa-bullhorn"
-        },
-        
-        {
-            title:"BANKING CONSULTING",
-            subtitle: "When while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed by when i hear the buzz of the title.",
-            icon: "fa-solid fa-wallet"
-        },
-        
-        {
-            title:"ENTERPRICE CONSULTING",
-            subtitle: "When while lovely valley teems with vapour around meand eridian sun strikes the upper impenetrable foliage of my trees, and but a thousand unknow plants are noticed by when i hear the buzz of the title.",
-            icon: "fa-solid fa-bullseye"
-        }
-
-    ]
+    
 })
