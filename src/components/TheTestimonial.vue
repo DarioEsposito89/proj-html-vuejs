b90265414c46a162b55742c810974a6a6276e26d
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-02 10:20:08 +0200

    creato componente TheTestimonial

---
+++ src/components/TheTestimonial.vue
@@ -0,0 +1,30 @@
+<!-- script section -->
+<script>
+
+export default{
+    // name
+    name:"TheAboutUs",
+
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<!-- /script section -->
+
+
+<!-- template section -->
+<template>
+    
+</template>
+<!-- /template section -->
+
+
+
+
+<style lang ="scss" scoped>
+
+</style>
+<!-- /style section -->
