cca833f6ea5d3cca2debe37b7531ca96e9d18161
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-03 10:25:20 +0200

    Creato e caricato il componente TheFooter

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
 import TheHeader from './components/TheHeader.vue';
 import TheMain from './components/TheMain.vue';
+import TheFooter from './components/TheFooter.vue';
 export default {
 
   components: {
     TheHeader,
     TheMain,
+    TheFooter,
   }
 }
 </script>
@@ -14,6 +16,7 @@ export default {
 <template>
   <TheHeader></TheHeader>,
   <TheMain></TheMain>,
+  <TheFooter></TheFooter>
 </template>
 
 <style lang="scss">
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,29 @@
+
+<script>
+
+export default{
+    // name
+    name: "TheFooter",
+    components: { 
+        
+    }
+}
+</script>
+
+
+<template>
+
+    
+    <footer id="contact">
+        
+    </footer>
+    
+
+</template>
+
+
+
+
+
+<style lang="scss" scoped>
+</style>
